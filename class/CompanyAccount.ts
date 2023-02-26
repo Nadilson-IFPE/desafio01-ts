c41ae7ff3a7ede66407c8cf13ede7a2445fe117e
Nadilson <nadilson@protonmail.com>
2023-02-26 01:17:04 -0300

    Desafio 1 do Formação TypeScript Fullstack Developer concluído

---
+++ class/CompanyAccount.ts
@@ -0,0 +1,14 @@
+import { DioAccount } from "./DioAccount"
+
+export class CompanyAccount extends DioAccount {
+
+  constructor(name: string, accountNumber: number) {
+    super(name, accountNumber)
+  }
+
+  getLoan(valor: number): void {
+    console.log(`${super.getName()} pegou empréstimo de R$ ${valor} e o depositou na sua conta`)
+
+    super.deposit(valor)
+  }
+}
---
+++ class/DioAccount.ts
@@ -0,0 +1,53 @@
+export abstract class DioAccount {
+  private readonly name: string
+  private readonly accountNumber: number
+  private balance: number = 0
+  private status: boolean = true
+
+  constructor(name: string, accountNumber: number) {
+    this.name = name
+    this.accountNumber = accountNumber
+  }
+
+  /* public setName = (name: string): void => {
+    this.name = name
+    console.log('Nome alterado com sucesso!')
+  } */
+
+  public getName(): string {
+    return this.name
+  }
+
+  public deposit(valor: number): void {
+    if (this.validateStatus()) {
+      this.balance += valor
+      console.log(`${this.getName()} depositou R$ ${valor} e seu saldo atual é R$ ${this.balance}`)
+    }
+  }
+
+  public withdraw(valor: number): void {
+    if (this.validateStatus()) {
+      if (this.balance >= valor) {
+        this.balance -= valor
+        console.log(`${this.getName()} sacou R$ ${valor} e seu saldo atual é R$ ${this.balance}`)
+      } else {
+        console.log(`Saldo insuficiente! ${this.getName()} tentou sacar R$ ${valor} de uma conta com saldo de R$ ${this.balance}`)
+      }
+    } else {
+      console.log('Conta inativa!')
+    }
+  }
+
+  public getBalance(): void {
+    console.log(`Saldo atual da conta de ${this.getName()}: R$ ${this.balance}`)
+
+  }
+
+  public validateStatus(): boolean {
+    if (this.status) {
+      return this.status
+    }
+
+    throw new Error('Conta inválida')
+  }
+}
